b1b139bd98ebb376b944fce8b56c7b096b9708ff
Boris Arzur <boris@konbu.org>
2023-06-08 14:13:27 +0200

    working, with temp sensor

--- MizuMachine.ino
+++ MizuMachine.ino
@@ -6,14 +6,21 @@
 // $ arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:pro:cpu=16MHzatmega328 KoujiMachine
 
 #include <inttypes.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define TEMPERATURE_PIN 4
+OneWire oneWire(TEMPERATURE_PIN);
+DallasTemperature sensor(&oneWire);
 
 #define LED(x) digitalWrite(13, x);
 
-#define OPEN(x) digitalWrite(2, x)
-#define CLOSE(x) digitalWrite(3, x)
+#define OPEN(x) digitalWrite(3, x)
+#define CLOSE(x) digitalWrite(2, x)
 
 #define CLOSED 0
 #define OPENED 1
+#define LOCKED 2
 
 void idle()
 {
@@ -26,24 +33,14 @@ void set_faucet(int direction)
   if (direction == OPENED) { CLOSE(0); OPEN(1); }
   else                     { OPEN(0); CLOSE(1); }
 
+  Serial.print((direction == OPENED)?"OPEN":"CLOSE");
+  Serial.println();
   delay(2000 /* ms */);
   idle();
 }
 
 #define UPDATE_LED 10000 /* ms */
-// 2
-//using seconds_t = int; // 21 B
-using seconds_t = unsigned long; // 25 B ok.
-
-//2 * sizeof(int) + x = 21
-//2 * sizeof(unsigned long) + x = 25
-// alignof?
-//template<int size_>
-//struct S { };
-//S<sizeof(int)>::wut;
-// -> sizeof(int) = 2
-//S<sizeof(unsigned long)>::wut;
-// -> sizeof(unsigned long) = 2
+using seconds_t = unsigned long;
 
 unsigned long start_ = 0;
 int seconds_ = 0;
@@ -54,26 +51,74 @@ int next_action_ = OPENED;
 
 void setup()
 {
+  Serial.begin(9600); // 9600 8N1
+  Serial.println("online");
+  delay(100);
+
+  // test routine
   set_faucet(CLOSED);
+  delay(1000);
+  set_faucet(OPENED);
+  delay(1000);
+  set_faucet(CLOSED);
+
+  next_time_ = 0 /* s */;
   next_action_ = OPENED;
-  next_time_ = 20 /* s */;
-  seconds_ = 0;
+  seconds_ = 1;
   start_ = millis();
 }
 
 void loop()
 {
-  const auto now = millis();
-  const auto delta = now - start_;
+  static int need_flush = 0;
+  struct X { ~X() { if (need_flush) { Serial.println(); } } } __x; // scope clean
+  need_flush = 0;
+#define SHOW(x)               \
+    need_flush = 1; \
+    Serial.print(" " #x ":"); \
+    Serial.print(x);
 
+  sensor.requestTemperatures();
+  const auto temp = sensor.getTempCByIndex(0);
+
+  // stop water when we dip below 22, restart when above 24.
+  const auto do_lock = (temp < 22.0) || ((next_action_ == LOCKED) && temp < 24.0);
+
+  SHOW(temp)
+  SHOW(do_lock)
+
+  if (do_lock) {
+    if (next_action_ != LOCKED) { /* we just locked */ set_faucet(CLOSED); }
+    next_action_ = LOCKED;
+    SHOW(next_action_)
+    delay(1000);
+    return;
+  }
+
+  const auto now = millis();
+
+  if (next_action_ == LOCKED) {
+    // we just unlocked.
+    next_action_ = OPENED;
+    // dont blink the led, do open the faucet just now.
+    next_time_ = 0;
+    seconds_ = 1;
+    start_ = now;
+  }
+
+  const auto delta = now - start_;
   if (delta > UPDATE_LED) {
     LED(led_);
     led_ = !led_;
     seconds_ += delta / 1000;
     start_ = now;
+
+    SHOW(next_action_)
+    SHOW(next_time_)
+    SHOW(seconds_)
   }
 
-  if (seconds_ > next_time_) {
+  if (seconds_ >= next_time_) {
     seconds_ = 0;
     if (next_action_ == CLOSED) {
       set_faucet(CLOSED);
@@ -86,5 +131,5 @@ void loop()
     }
   }
 
-  delay(100);
+  delay(10001);
 }
